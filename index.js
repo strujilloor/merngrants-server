f4ebf2698b011d7b54aa7d6dc2195412ab505346
Stiven Trujillo <strujillo1293@gmail.com>
2020-06-03 22:04:04 -0500

    connecting mongodb to our app

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 // create server
 const app = express();
+
+// Connect to DB
+connectDB();
 
 // App port
 const PORT = process.env.PORT || 4000;
