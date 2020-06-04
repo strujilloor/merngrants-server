90a4974e13e9af0f5a2a13c82e90df04291f5e12
Stiven Trujillo <strujillo1293@gmail.com>
2020-06-03 21:27:07 -0500

    Creating the express server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('desde index.js');
+const express = require('express');
+
+// create server
+const app = express();
+
+// App port
+const PORT = process.env.PORT || 4000;
+
+// Define main page
+// app.get('/', (req, res) => {
+//     res.send('Hola mundo');
+// });
+
+// Run app
+app.listen(PORT, () => {
+    console.log(`server run on port ${PORT}`);
+})
