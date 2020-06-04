141ca982a6dc3b025d452db586ab57163333a35a
Stiven Trujillo <strujillo1293@gmail.com>
2020-06-04 00:54:46 -0500

    express.json, bodyParser substitute

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect to DB
 connectDB();
 
+// Enable express.json
+app.use( express.json({ extended: true }) ); // bodyParser substitute
+
 // App port
 const PORT = process.env.PORT || 4000;
 
@@ -16,5 +19,5 @@ app.use('/api/grants', require('./routes/grants'));
 
 // Run app
 app.listen(PORT, () => {
-    console.log(`server run on port ${PORT}`);
+    console.log(`Server running on port: ${PORT}`);
 })
