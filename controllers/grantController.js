252710dd63a990a05cc5b488082c10e7535d6d8b
Stiven Trujillo <strujillo1293@gmail.com>
2020-06-06 23:35:46 -0500

    change the way to save an array

--- controllers/grantController.js
+++ controllers/grantController.js
@@ -30,7 +30,7 @@ exports.updateGrants = async (req, res) => {
 
     try {
         // Get grants from grants.gov
-        const response = await axios.post('https://www.grants.gov/grantsws/rest/opportunities/search/', {startRecordNum: 0, sortBy: "openDate|desc", oppStatuses: "forecasted|posted", rows: 10});
+        const response = await axios.post('https://www.grants.gov/grantsws/rest/opportunities/search/', {startRecordNum: 0, sortBy: "openDate|desc", oppStatuses: "forecasted|posted", rows: 1000});
         // note: change the amount of data with rows (1000)
 
         const grants = response.data.oppHits;
@@ -39,18 +39,7 @@ exports.updateGrants = async (req, res) => {
         let newGrant;
         let grantExists
 
-        grants.forEach( async (grant) => {
-            // validate that the grant is unique
-            grantExists = await Grant.findOne({ id: grant.id });
-            if ( !grantExists ) {
-                // create new grant
-                newGrant = new Grant({ ...grant });
-                // save grant
-                await newGrant.save();
-            } else {
-                console.log('Grant already exist');
-            }
-        });
+        await Grant.insertMany( grants );
 
         // confirmation message
         res.json({ msg: 'Grants Updated' });
